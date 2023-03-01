f8b099eee0ef8bd46b72af9dceb671d782e1b34b
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-03-01 17:16:41 +0300

    Norma

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React dcasdcas
+          START!!!!!!!!!!!
         </a>
       </header>
     </div>
