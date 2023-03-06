8cf56a8d318c94a8542c2ed6ec4297c0695647b4
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-03-06 19:02:43 +0300

    77777

--- src/Kata/kata6_order.js
+++ src/Kata/kata6_order.js
@@ -1,12 +1,17 @@
-//"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
-//"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
-//""  -->  ""
-function order(words){
-    return words.split(' ').sort(function(a, b){
-        return a.match(/\d/) - b.match(/\d/);
-        console.log(a.match(/\d/))
-        console.log('a.match(/\d/)')
-    }).join(' ');
-}
-order('is2 Thi1s T4est 3a')
+class Animal {
 
+    constructor(name,type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    toString() {
+        console.log(`${this.name} is a ${this.type}`);
+        return `${this.Max} "is a dog" ${this.name}`
+    }
+
+}
+
+// Использование:
+let dog = new Animal('Sat','cat');
+dog.toString();//образится: "Rectangle
