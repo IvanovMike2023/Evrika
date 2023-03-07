a80d15ffa24e331d66663a03bedfb6b2628d5dac
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-03-07 15:59:47 +0300

    string repeat4

--- src/Kata/string_repeat.js
+++ src/Kata/string_repeat.js
@@ -1,10 +1,23 @@
 import React from "react";
- function highAndLow(n,s) {
 
-    return console.log(s.repeat(n))
+function keepOrder(arr, val) {
+    for (let i = 0; i < arr.length; i++){
+        if ( val <= arr[i] ) {
+            return console.log(i);
+        }
+    }
+    return arr.length;
+};
 
-}
-
-highAndLow(3,"*");  // return "5 1"
-// highAndLow("1 2 -3 4 5"); // return "5 -3"
-// highAndLow("1 9 3 4 -5"); // return "9 -5"
+keepOrder([-4, 0, 2, 2, 5], -4 );  // return "5 1"
+//
+//  Test.assertSimilar(keepOrder([1, 2, 3, 4, 7], 5), 4);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4, 7], 0), 0);
+//     Test.assertSimilar(keepOrder([1, 1, 2, 2, 2], 2), 2);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4], 5), 4);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4], -1), 0);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4], 2), 1);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4], 0), 0);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4], 1), 0);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4], 2), 1);
+//     Test.assertSimilar(keepOrder([1, 2, 3, 4], 3), 2);
