2e99f0621d386c8bc3722e29405e10bf647dbce4
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2023-03-07 19:13:48 +0300

    string rep3333333

--- src/Kata/string_repeat.js
+++ src/Kata/string_repeat.js
@@ -1,6 +1,9 @@
 import React from "react";
 function validPhoneNumber(phoneNumber){
     console.log(phoneNumber)
+    console.log(phoneNumber)
+    console.log(phoneNumber)
+    console.log(phoneNumber)
     return
 }
 validPhoneNumber ("(123) 456-7890")
